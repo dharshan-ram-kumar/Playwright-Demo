b275242b45596dae88b55b38dea9dda929f611e9
dharshan-ram-kumar <dharshanram.k@tringapps.net>
2025-06-05 16:43:47 +0530

    update test cases

--- tests/annotation.spec.ts
+++ tests/annotation.spec.ts
@@ -1,7 +1,20 @@
 import { test, expect } from "@playwright/test";
 import { baseURl } from "../config";
 
+test("Conditional skip based on env", async ({ page }) => {
+  test.skip(process.env.SKIP_CASE === "true", "Skipping due to env var");
+  await page.goto(process.env.baseURL || baseURl);
+});
+
 test.skip("Skip Navigate to Orange HRM", async ({ page }) => {
+  await page.goto(process.env.baseURL || baseURl);
+  await expect(page).toHaveTitle("OrangeHRM");
+});
+
+test.only("Based on browser, Navigate to Orange HRM", async ({
+  page,
+}, browser) => {
+  test.skip(browser.project.name !== "chromium", "Only for Chromium");
   await page.goto(process.env.baseURL || baseURl);
   await expect(page).toHaveTitle("OrangeHRM");
 });
@@ -22,10 +35,15 @@ test("Slowdown Navigate to Orange HRM", async ({ page }) => {
   await expect(page).toHaveTitle("OrangeHRM");
 });
 
-test.describe("Describe Navigate to Orange HRM", () => {
-  test("Navigate to Orange HRM", async ({ page }) => {
-    await page.goto(process.env.baseURL || baseURl);
-    await expect(page).toHaveTitle("OrangeHRM");
+test.describe("Grouped test", () => {
+  test("Test 1", async ({ page }) => {
+    await page.goto("/");
+    await expect(page).toHaveTitle(/OrangeHRM/);
+  });
+
+  test("Test 2", async ({ page }) => {
+    await page.goto("/");
+    await expect(page.locator("h5")).toHaveText("Login");
   });
 });
 
