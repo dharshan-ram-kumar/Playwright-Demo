4d2865681dce1e4b95e47b74d9a9b913d2810652
dharshan-ram-kumar <dharshanram.k@tringapps.net>
2025-06-05 16:56:08 +0530

    Updated test cases

--- tests/annotation.spec.ts
+++ tests/annotation.spec.ts
@@ -3,34 +3,34 @@ import { baseURl } from "../config";
 
 test("Conditional skip based on env", async ({ page }) => {
   test.skip(process.env.SKIP_CASE === "true", "Skipping due to env var");
-  await page.goto(process.env.baseURL || baseURl);
+  await page.goto(process.env.baseURL ?? baseURl);
 });
 
 test.skip("Skip Navigate to Orange HRM", async ({ page }) => {
-  await page.goto(process.env.baseURL || baseURl);
+  await page.goto(process.env.baseURL ?? baseURl);
   await expect(page).toHaveTitle("OrangeHRM");
 });
 
-test.only("Based on browser, Navigate to Orange HRM", async ({
+test("Based on browser, Navigate to Orange HRM", async ({
   page,
 }, browser) => {
   test.skip(browser.project.name !== "chromium", "Only for Chromium");
-  await page.goto(process.env.baseURL || baseURl);
+  await page.goto(process.env.baseURL ?? baseURl);
   await expect(page).toHaveTitle("OrangeHRM");
 });
 
 test.fail("Fail test", async ({ page }) => {
-  await page.goto(process.env.baseURL || baseURl);
+  await page.goto(process.env.baseURL ?? baseURl);
   await expect(page).toHaveTitle(/Login/);
 });
 
 test("@focus Run only Navigate to Orange HRM", async ({ page }) => {
-  await page.goto(process.env.baseURL || baseURl);
+  await page.goto(process.env.baseURL ?? baseURl);
   await expect(page.locator("h5")).toHaveText("Login");
 });
 
 test("Slowdown Navigate to Orange HRM", async ({ page }) => {
-  await page.goto(process.env.baseURL || baseURl);
+  await page.goto(process.env.baseURL ?? baseURl);
   test.slow();
   await expect(page).toHaveTitle("OrangeHRM");
 });
@@ -49,7 +49,7 @@ test.describe("Grouped test", () => {
 
 test("Test with errors", async ({ page }) => {
   try {
-    await page.goto(process.env.baseURL || baseURl);
+    await page.goto(process.env.baseURL ?? baseURl);
     await expect(page).toHaveTitle("HRMS");
   } catch (error) {
     test.fixme(true, "Test failed unexpectedly, skipping...");
